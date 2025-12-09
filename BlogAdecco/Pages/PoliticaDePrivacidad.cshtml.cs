02284bf29a2d781a7422d0219a2ddc0a0123c81f
Samuel Kobelkowsky <sk@kobelsoft.com>
2025-12-09 16:37:19 -0600

    Políticas de cookies y de privacidad

---
+++ BlogAdecco/Pages/PoliticaDePrivacidad.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogAdecco.Pages
+{
+    public class PoliticaDePrivacidadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
