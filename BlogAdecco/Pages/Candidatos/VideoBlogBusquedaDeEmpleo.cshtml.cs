2ed251e3c5096ef9048f395ef8697cd8a2649fc1
Samuel Kobelkowsky <sk@kobelsoft.com>
2025-12-10 20:51:37 -0600

    Se convierte header de candidatos y empresas a algo que se pueda reutlizar en otras vistas

--- BlogAdecco/Pages/Candidatos/VideoBlogBusquedaDeEmpleo.cshtml.cs
+++ BlogAdecco/Pages/Candidatos/VideoBlogBusquedaDeEmpleo.cshtml.cs
@@ -4,6 +4,7 @@
 // Email: samuel@mapachedigital.com
 
 using BlogAdecco.Api.Responses;
+using BlogAdecco.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -17,6 +18,26 @@ public class VideoBlogBusquedaDeEmpleoModel(IConfiguration _configuration, IHttp
 
     public async Task<IActionResult> OnGet()
     {
+        var headerData = new HeaderData
+        {
+            Template = "CandidatesCompanies",
+            Subtitle = "Aspirantes",
+            Email = "calidadmx@adecco.com",
+            Phone = "800 832 9050",
+            Breadcrumbs = ["Video blog para la búsqueda de empleo"],
+            Links1 = [
+                new() { Title = "Consejos para el desarrollo profesional", Width = 220, Url = Url.Page("/Categoria", new { Category = "candidatos", SubCategory1 = "consejos-para-el-desarrollo-profesional" }) },
+                new() { Title = "Derechos y prestaciones laborales", Width = 220, Url = Url.Page("/Categoria", new { Category = "candidatos", SubCategory1 = "derechos-prestaciones-laborales" }) },
+                new() { Title = "Tips para entrevistas de trabajo", Width = 220, Url = Url.Page("/Categoria", new { Category = "candidatos", SubCategory1 = "tips-para-entrevistas-de-trabajo" }) },
+            ],
+            Links2 = [
+                new() { Title = "Infografías", Width = 120, Url = Url.Page("/Categoria", new { Category = "candidatos", SubCategory1 = "infografias" }) },
+                new() { Title = "Recursos digitales", Width = 120, Url = Url.Page("/Candidatos/RecursosDigitalesCandidatos") },
+                new() { Title = "Video Blog", Width = 100, Url = Url.Page("/Candidatos/VideoBlogBusquedaDeEmpleo"), Enabled = false },
+            ]
+        };
+
+        ViewData["Header"] = headerData;
         var maxResults = 48;
         var apiKey = _configuration[Globals.ConfigGoogleApiKey];
         var url = $"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={maxResults}&playlistId={Globals.YoutubePlaylistCandidates}&key={apiKey}";
--- BlogAdecco/Pages/Empresas/VideoBlogRecursosHumanos.cshtml.cs
+++ BlogAdecco/Pages/Empresas/VideoBlogRecursosHumanos.cshtml.cs
@@ -5,42 +5,62 @@
 
 using BlogAdecco.Api.Responses;
 using BlogAdecco.Pages.Candidatos;
+using BlogAdecco.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BlogAdecco.Pages.Empresas
+namespace BlogAdecco.Pages.Empresas;
+
+public class VideoBlogRecursosHumanosModel(IConfiguration _configuration, IHttpClientFactory _httpClientFactory, ILogger<VideoBlogBusquedaDeEmpleoModel> _logger) : PageModel
 {
-    public class VideoBlogRecursosHumanosModel(IConfiguration _configuration, IHttpClientFactory _httpClientFactory, ILogger<VideoBlogBusquedaDeEmpleoModel> _logger) : PageModel
+    public List<Video> Videos { get; set; } = [];
+
+    public bool FailedToLoadVideos { get; set; } = false;
+
+    public async Task<IActionResult> OnGet()
     {
-        public List<Video> Videos { get; set; } = [];
-
-        public bool FailedToLoadVideos { get; set; } = false;
-
-        public async Task<IActionResult> OnGet()
+        var headerData = new HeaderData
         {
-            var maxResults = 48;
-            var apiKey = _configuration[Globals.ConfigGoogleApiKey];
-            var url = $"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={maxResults}&playlistId={Globals.YoutubePlaylistCompanies}&key={apiKey}";
-            var client = _httpClientFactory.CreateClient();
-            GoogleApiResponse? result;
+            Template = "CandidatesCompanies",
+            Subtitle = "Empresas",
+            Email = "atencionaclientes@adecco.com",
+            Phone = "800-890-0173",
+            Breadcrumbs = ["Video blog para recursos humanos"],
+            Links1 = [
+                new() { Title = "Maquila de nómina", Width = 130, Url = Url.Page("/Categoria", new { Category = "empresas", SubCategory1 = "maquila-de-nomina"}) },
+                new() { Title = "Reclutamiento de personal", Width = 160, Url = Url.Page("/Categoria", new { Category = "empresas", SubCategory1 = "reclutamiento-outsourcing-de-personal"}) },
+                new() { Title = "Capacitación de personal", Width =  180, Url = Url.Page("/Categoria", new { Category = "empresas", SubCategory1 = "capacitacion-de-personal"}) },
+            ],
+            Links2 = [
+                new() { Title = "Recursos digitales", Width = 106, Url = Url.Page("/Empresas/RecursosDigitalesEmpresas") },
+                new() { Title = "Video Blog", Width =  80, Url = Url.Page("/Empresas/VideoBlogRecursosHumanos"), Enabled = false},
+            ]
+        };
 
-            try
-            {
-                result = await client.GetFromJsonAsync<GoogleApiResponse>(url);
-                Videos = result?.Videos ?? [];
-            }
-            catch (HttpRequestException ex)
-            {
-                // Log error
-                if (_logger.IsEnabled(LogLevel.Critical))
-                {
-                    _logger.LogCritical(ex, "Error fetching YouTube playlist: {message}. Check the API Key and playlist Id.", ex.Message);
-                }
+        ViewData["Header"] = headerData;
 
-                FailedToLoadVideos = true;
-            }
+        var maxResults = 48;
+        var apiKey = _configuration[Globals.ConfigGoogleApiKey];
+        var url = $"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={maxResults}&playlistId={Globals.YoutubePlaylistCompanies}&key={apiKey}";
+        var client = _httpClientFactory.CreateClient();
+        GoogleApiResponse? result;
 
-            return Page();
+        try
+        {
+            result = await client.GetFromJsonAsync<GoogleApiResponse>(url);
+            Videos = result?.Videos ?? [];
         }
+        catch (HttpRequestException ex)
+        {
+            // Log error
+            if (_logger.IsEnabled(LogLevel.Critical))
+            {
+                _logger.LogCritical(ex, "Error fetching YouTube playlist: {message}. Check the API Key and playlist Id.", ex.Message);
+            }
+
+            FailedToLoadVideos = true;
+        }
+
+        return Page();
     }
 }
